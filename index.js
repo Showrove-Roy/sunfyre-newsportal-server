ca276a6fbf1aed1cd10790553bd7d6b8a965b4e8
Showrove Roy <showrove200@gmail.com>
2022-10-27 07:16:07 +0600

    news and cateroty url generate

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const app = express();
 const port = 5000;
 const categories = require("./data/Category.json");
+const news = require("./data/news.json");
 
 app.use(cors());
 app.get("/", (req, res) => {
@@ -11,6 +12,20 @@ app.get("/", (req, res) => {
 
 app.get("/categories", (req, res) => {
   res.send(categories);
+});
+app.get("/category/:id", (req, res) => {
+  const id = req.params.id;
+  if (id === "08") {
+    res.send(news);
+  } else {
+    const categoryNews = news.filter((n) => n.category_id === id);
+    res.send(categoryNews);
+  }
+});
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  const selectedNews = news.find((n) => n._id === id);
+  res.send(selectedNews);
 });
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
